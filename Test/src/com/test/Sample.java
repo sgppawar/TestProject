2a1443164b5c6e0dd68f1bdf505a26e771298f5a
suraj pawar <surajpawar1432@gmail.com>
2023-04-28 13:27:56 +0530

    Added 1st line

---
+++ Test/src/com/test/Sample.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Sample {
+
+		
+		public static void main(String[] args) {
+			
+			for(int i=1; i<10; i++) {
+			System.out.println("Git Demo");
+			}
+		}
+
+	}
+
+
